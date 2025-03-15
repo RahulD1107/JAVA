a76047c39f578403127761dbdcdc57246716e7e6
RahulD1107 <rahuldurai020@gmail.com>
2025-03-15 23:26:12 +0530

    Element appear at once

--- java.java
+++ java.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;
+class java{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        char []arr=new char[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scan.next().charAt(0);
+        }
+          char  []unique=new char[n];
+         
+        int r=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            
+            int freq=0;
+            for(int j=0;j<n;j++){
+                 if (arr[i]==arr[j])
+                  freq++;
+                  }
+            if(freq==1){
+              System.out.print(arr[i]+ " ");
+            }
+        }
+    }
+}
