1817ee2208f6b463136c8a3c0742679fd489589e
RahulD1107 <rahuldurai020@gmail.com>
2025-03-24 01:19:32 +0530

    Double same adjacent values, replace next with 0

---
+++ Double.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Double{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scan.nextInt();
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==arr[i+1]){
+                arr[i]+=arr[i+1];
+                arr[i+1]=0;
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+        
+       //input  is [1 ,2 ,2, 3 ,4 ]
+       //output is [1,4,0,3,4]
+    }
+}
