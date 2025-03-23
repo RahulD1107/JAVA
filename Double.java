4c744dbc5bff80fa1d72a8fb1518c3b4a95eb9e1
RahulD1107 <rahuldurai020@gmail.com>
2025-03-24 01:25:16 +0530

    Double same adjacent values, replace next with 0

--- Double.java
+++ Double.java
@@ -10,7 +10,7 @@ class Double{
 
         for(int i=0;i<n-1;i++){
             if(arr[i]==arr[i+1]){
-                arr[i]+=arr[i+1];
+                arr[i]*=2;
                 arr[i+1]=0;
             }
         }
